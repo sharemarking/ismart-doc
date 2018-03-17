c80550ec183cc5bac8a9304ca5127ae295c226ea
sbness <93220077@qq.com>
2018-03-17 19:54:18 +0800

    ismart-doc fork and edit

---
+++ example/ismart.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    doc: {
+        name         : "ismart-path",
+        title        : "Path扩展-ismart",
+        version      : "0.1.0",
+        repositoryUrl: "https://github.com/hnzxb/ismart-path",
+        introduction : '描述',
+        paths        : {
+            input : "./src",
+            output: "./readme"
+        },
+        source       : '',
+        skip         : []
+    }
+}
